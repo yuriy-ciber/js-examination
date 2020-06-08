3c6dc5428a262fe215dcccfdc124dd2ab72efd13
yuriy-ciber <yuriylob72@gmail.com>
2020-06-08 11:59:13 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,49 @@
+<p align="center">
+  <a href="https://gulpjs.com">
+    <img height="257" width="114" src="https://raw.githubusercontent.com/gulpjs/artwork/master/gulp-2x.png">
+  </a>
+  <h1 align="center">Gulp config</h1>
+</p>
+
+## Installation
+
+```bash
+# Create new folder and install the gulp project there
+npx degit a1exalexander/gulp <my-new-project>
+cd <my-new-project>
+npm install
+
+# or install the gulp project here
+npx degit a1exalexander/gulp .
+npm install
+```
+
+## Usage
+
+```bash
+# Run devserver
+npm start
+# or run deveserver with HTML validator
+npm run dev
+```
+
+```bash
+# Build for production
+npm run build
+```
+
+### Project structure
+    .
+    ├── src                    # Source files
+      ├── fonts
+      ├── images
+      ├── js                   # Entry JS files
+      ├── styles               # CSS, SASS, SCSS style files (ignore: '_')
+      index.html
+
+
+#### Rigger (import HTML components)
+```
+//= templates/_foo.html
+//= _bar.html
+```
---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const $burger = document.querySelector(".header__burger");
+  const $menu = document.querySelector(".header__menu");
+  const $line = document.querySelector(".header__burger-line");
+  const $link = document.querySelector(".header__link");
+  const $list = document.querySelector(".header__list");
+  const $body = document.getElementsByTagName("body");
+  console.log($body);
+  // console.log(event);
+  const addActive = () => {
+    $burger.classList.toggle("header__burger--active");
+    $line.classList.toggle("header__burger-line--active");
+    $menu.classList.toggle("header__menu--active");
+    $body.classList.toggle("body__lock");
+
+    //   console.log(event);
+  };
+  $burger.onclick = addActive;
+
+  const removeActive = () => {
+    $burger.classList.remove("header__burger--active");
+    $line.classList.remove("header__burger-line--active");
+    $menu.classList.remove("header__menu--active");
+    console.log(event);
+  };
+  $list.onclick = removeActive;
+});
